79d1c30fc33023df776eb7cc70376b50131e7305
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-13 21:40:06 +0800

    correction

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -4,6 +4,15 @@ Created on Wed Mar 13 10:15:36 2019
 
 @author: lenovo
 """
-#start with a positive integer x (x is no larger than 8192 = 213)
+#start with a positive integer x (x is no larger than 8192 = 2**13)
 x = 2019
-
+i = 13
+a = str(2**i)
+b = str(x )
+while x != 0:
+    if x < 2**i:
+        i=i-1
+    else:
+        output = a + "+"
+        x=x-2**i
+print(b + output)
