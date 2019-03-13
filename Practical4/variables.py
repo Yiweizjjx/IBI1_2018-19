14c30c81ddab53f0ab96ea221dd51512ac700392
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-13 10:36:02 +0800

    add file "variables"

---
+++ Practical4/variables.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:15:22 2019
+
+@author: lenovo
+"""
+
+a = 233
+b = 233233
+print(b%7)
+c = b/7
+d = c/11
+e = d/13
+print(a < e)
+print(a == e)
+print(a > e)
+
+
+X = True
+Y = True
+Z = (X and not Y) or (Y and not X)
+W = X != Y
+print(Z)
+print(W)
