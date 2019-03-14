4c9143cadd5e84ee3e3791aa46384fb3af4ca68e
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-14 19:09:36 +0800

    anwer for mystery_code

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -9,17 +9,23 @@ from random import randint
 # ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
-
+# make a Boolean variable
 p=False
+# loop
 while p==False:
     p=True
+# draw random integer from 1 to 100    
     n = randint(1,100)
+# take the ceiling of √n
     u = ceil(n**(0.5))
+# loop   
     for i in range(2,u+1):
+# test if n is a prime number        
         if n%i == 0:
+# n is not a prime number, continue the loop            
             p=False
 
 
-     
+ # output the prime number    
 print(n)
             
