8f6715d37c3fd5a993fdf7bc1c4033d05c2582c6
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-14 20:18:10 +0800

    answer for powersof2

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -4,15 +4,27 @@ Created on Wed Mar 13 10:15:36 2019
 
 @author: Yiwei
 """
-#start with a positive integer x (x is no larger than 8192 = 2**13)
+# start with a positive integer x (x is no larger than 8192 = 2**13)
 x = 2019
-i = 13
-a = str(2**i)
-b = str(x)
-while x != 0:
-    if x < 2**i:
-        i=i-1
-    else:
-        output = a + "+"
+# make the first part of the sentence
+y = str(x) + " is" 
+# define a as string
+a = str()
+# loop (i can take 13,12,...,2,1)
+for i in range(13,0,-1):
+# find the biggest 2**i that is smaller than x
+    if x >= 2**i:
+# keep record
+        a=a + "2**" + str(i) 
+# subtract 2**i from x to find the next biggest 2**i that is smaller than x        
         x=x-2**i
-print(b + output)
+# check if x is equal to 0
+# if x is equal to 0, then break
+# if x is not equal to 0, add " + " to link
+        if x!=0:
+         a=a + " + "  
+# if x is equal to 1, then the last part should be "2**0"
+    if x==1:
+           a=a + "2**" + str(0)
+# output the sentence
+print(y,a) 
