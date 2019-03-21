65dc53b8cb0476fa025c0a5e42cd1a33f8c38bc0
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-21 11:20:23 +0800

    answer for string reverse and list sorting

--- practicel5/string reverse and list sorting.py
+++ practicel5/string reverse and list sorting.py
@@ -7,12 +7,9 @@ Created on Wed Mar 20 09:51:42 2019
 
 s = "but soft what light in yonder window breaks"
 L1 = s.split(" ")
-L2 = L1.copy()
-L3 = []
-for i in L2:
-    L1.remove(i)
-    i = i[::-1]
-    L3.append(i)
-L3.sort()
-L3.reverse()
-print(L3)
+L2 = []
+for i in L1:
+    L2.append(i[::-1])
+L2.sort()
+L2.reverse()
+print(L2)
