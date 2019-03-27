d35a08322d4743680471f82736db21f3fd56b094
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-27 21:14:25 +0800

    almost finished

--- practical6/sending emails.py
+++ practical6/sending emails.py
@@ -13,12 +13,28 @@ address_list = []
 subject_list = []
 for a in fhand:
     mylist = re.split(r',', str(a))
-    if re.search(r'@', mylist[1]) == None:
-       continue
-    if re.match(r'.com', mylist[1]):
+    if re.search(r'.*@.*', mylist[1]):
+       if re.match(r'.*com.*', mylist[1]):
          print(mylist[1], ':Correct Address!')
-         name_list = name_list + [mylist[0]]
+         name_list += [mylist[0]]
          address_list += [mylist[1]]
          subject_list += [mylist[2]]
-    else:
+       else:
          print(mylist[1], ':Wrong Address!')
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+fhand1 = open('body.txt', 'r') 
+inp = fhand1.read()
+for i in range(0, len(address_list)):    
+    from_address = '3180111428@zju.edu.cn'
+    to_address = address_list[i]
+    body = re.sub(r'User', name_list[i], inp)
+    msg = MIMEText(body, 'plain', 'utf-8')
+    msg['From'] = Header("3180111428", 'utf-8')
+    msg['To'] =  Header(name_list[i], 'utf-8')
+    msg['Subject'] = Header(subject_list[i], 'utf-8')    
+    text = msg.as_string()
+
+print(body)
