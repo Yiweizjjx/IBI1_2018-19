771cd3aeb71db42a4275953fb9740cd6ec15c835
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-03-27 11:51:44 +0800

    not finished

--- practical6/sending emails.py
+++ practical6/sending emails.py
@@ -4,4 +4,21 @@ Created on Wed Mar 27 09:01:57 2019
 
 @author: Yiwei
 """
-
+import re
+fhand = open('address_information.csv', 'r')
+x = str() 
+L1 = []
+name_list = []
+address_list = []
+subject_list = []
+for a in fhand:
+    mylist = re.split(r',', str(a))
+    if re.search(r'@', mylist[1]) == None:
+       continue
+    if re.match(r'.com', mylist[1]):
+         print(mylist[1], ':Correct Address!')
+         name_list = name_list + [mylist[0]]
+         address_list += [mylist[1]]
+         subject_list += [mylist[2]]
+    else:
+         print(mylist[1], ':Wrong Address!')
