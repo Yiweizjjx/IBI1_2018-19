c2438da8a4cc5fabea0c37afecf650cc1374466d
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-04-11 15:20:00 +0800

    improved version

--- practical8/find go terms.py
+++ practical8/find go terms.py
@@ -5,10 +5,10 @@ Created on Wed Apr 10 09:04:54 2019
 @author: Yiwei
 """
 
-from xml.dom.minidom import parse  
 import xml.dom.minidom
 import re
-
+import pandas as pd
+'''
 DOMTree = xml.dom.minidom.parse("go_obo.xml")
 collection = DOMTree.documentElement 
 defstr = collection.getElementsByTagName("defstr")
@@ -26,3 +26,43 @@ for i in range(0,len(defstr)):
    i = i+1
 print(L1)
 print(L2)
+'''
+###
+filePath=r'D:\IBI1\practical'
+resName='autophagosome.xlsx'
+res=filePath+'/'+resName
+
+
+def Child(id, resultSet):
+    for t in go:
+        parents = t.getElementsByTagName('is_a')
+# parents is a list of elements        
+        geneid = t.getElementsByTagName('id')[0].childNodes[0].data
+        for parent in parents:
+            if parent.childNodes[0].data == id:
+                resultSet.add(geneid)
+                Child(geneid,resultSet)
+# create a pandas.Dataframe to store the output               
+df = pd.DataFrame(columns=['id','name','definition','childnodes'])
+
+#create the DOM tree
+DOMTree = xml.dom.minidom.parse("go_obo.xml")
+collection = DOMTree.documentElement 
+go = collection.getElementsByTagName("term")
+for term in go:
+    defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
+    # find terms that contain the word 'autophagosome'
+    if re.search(r'autophagosome+', defstr):
+        id = term.getElementsByTagName('id')[0].childNodes[0].data
+        name = term.getElementsByTagName('name')[0].childNodes[0].data
+        defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
+        resultSet = set()
+        # Use set! once the item  was found,it can only be counted once
+        # reduce redundancy
+        Child(id, resultSet)
+        df = df.append(pd.DataFrame({'id':[id], 'name':[name], 'definition':[defstr], 'childnodes':[len(resultSet)]}))
+        print(id, len(resultSet))
+
+df.to_excel(res, index=False)
+
+        
