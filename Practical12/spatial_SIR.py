88d1b3295e50364e55a873dd80db7d905805cf54
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-14 23:09:18 +0800

    improvements for codes and comments added

--- Practical12/spatial_SIR.py
+++ Practical12/spatial_SIR.py
@@ -4,6 +4,17 @@ Created on Mon May 13 21:42:11 2019
 
 @author: Yiwei
 """
+
+'''
+1. find infected individuals
+2. infect 8 neighbours with infection probability (0.3)
+   make sure the infected person does not infect himself or infect neighbours beyond the edge
+3. infected individuals may recover
+   find infected individuals again
+   for each infected individual, determine if it will recover with recovery probability (0.05)
+4. plot the outcomes from t=0 to 100
+      
+'''
 # import necessary libraries 
 import numpy as np 
 import matplotlib . pyplot as plt
@@ -17,29 +28,29 @@ plt.imshow(population, cmap='viridis', interpolation='nearest')
 beta=0.3
 gamma=0.05
 
-for m in range(0,20):
+for m in range(0,100):
 # find infected points
   inf = np.where(population==1)
 # loop through all infected points
-  for i in range(len(inf[0])):
+  for i in range(0,len(inf[0])):
     # get x, y coordinates for each point
     x = inf[0][i]
     y = inf[1][i]
-    # infect each neighbour with probability beta
-    # infect all 8 neighbours (this is a bit finicky, is there a better way?):
+    # infect all 8 neighbours with probability beta
     for xNeighbour in range(x-1,x+2):
         for yNeighbour in range(y-1,y+2):
-            # don't infect yourself! (Is this strictly necessary?)
+            # avoid infecting youself 
             if (xNeighbour,yNeighbour) != (x,y):
-                # make sure I don't fall off an edge
+                # avoid infecting neighbours beyond the edge
                 if xNeighbour != -1 and yNeighbour != -1 and xNeighbour!=100 and yNeighbour!=100:
-                    # only infect neighbours that are not already infected!
+                    # only infect neighbours that are not already infected
                     if population[xNeighbour,yNeighbour]==0:
                         population[xNeighbour,yNeighbour]=np.random.choice(range(2),1,p=[1-beta,beta])[0]
-    inf = np.where(population==1)
-    for j in range(0,len(inf[0])):
-        a=np.random.choice(range(2),1,p=[1-gamma,gamma])[0]
-    if a==1:
-        population[inf[0][j],inf[1][j]]=0.5
+  inf = np.where(population==1)
+  for j in range(0,len(inf[0])):
+      # infected individuals may recover with probability gamma
+      a=np.random.choice(range(2),1,p=[1-gamma,gamma])[0] 
+      if a==1:
+          population[inf[0][j],inf[1][j]]=0.5 # the person get recovered
   plt.figure(figsize =(6,4), dpi=150) 
   plt.imshow(population, cmap='viridis', interpolation='nearest')
