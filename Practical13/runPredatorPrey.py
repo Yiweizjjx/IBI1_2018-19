7d23517221d9676dc61c947704cc8c3ea8218a0e
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 17:03:07 +0800

    not finished

--- Practical13/runPredatorPrey.py
+++ Practical13/runPredatorPrey.py
@@ -4,6 +4,7 @@ Created on Wed May 15 09:08:34 2019
 
 @author: Yiwei
 """
+
 import os
 '''
 os.chdir('D:/IBI1/practical/IBI1_2018-19/Practical13') 
@@ -58,4 +59,52 @@ def xml_to_cps():
 
 xml_to_cps()
 '''
-os.system('D:/ICMB1/practical/COPASI 4.24.197/bin/CopasiSE predator−prey.cps')
+#os.system('CopasiSE.exe predator−prey.cps')
+
+import numpy as np
+import matplotlib . pyplot as plt
+
+data=open('modelResults.csv', 'r')
+count=0
+results=[[],[],[]]
+for line in data:
+    line=line.rstrip()
+    count+=1
+    if count==1:
+        variable=line.split(',')
+        names=np.array([variable])
+    else:
+        result=line.split(',')
+        results[0].append(result[0])
+        results[1].append(result[1])
+        results[2].append(result[2])
+        
+results = np.array(results)
+results = results.astype(np.float)
+# plot a time course of the predator and prey population
+plt.title('Time course')
+plt.plot(results[0],results[1],label='Predator (b=0.02, d=0.4')
+plt.plot(results[0],results[2],label='Prey (b=0.1, d=0.02)')   
+plt.xlabel('time')
+plt.ylabel('population')
+plt.legend()
+plt.show()
+#  plot predator population against prey population
+plt.title('Limit cycle')
+plt.plot(results[2],results[1])
+plt.xlabel('predator popluation')
+plt.ylabel('prey popluation')
+plt.show()
+
+import xml.dom.minidom
+'''
+k_predator_breeds=0.1
+k_predator_dies=0.2
+k_prey_breeds=0.3
+k_prey_dies=0.4
+'''
+DOMTree = xml.dom.minidom.parse("predator-prey.xml")
+collection = DOMTree.documentElement 
+para = collection.getElementsByTagName("listOfParameters")
+for j in para:
+    setAttribute
