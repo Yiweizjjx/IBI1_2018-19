b04e229d0c81d74b47b13704d1aff5741c6c564e
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 21:52:10 +0800

    read matrix

--- practical9/Alignment.py
+++ practical9/Alignment.py
@@ -4,28 +4,15 @@ Created on Wed Apr 17 09:12:01 2019
 
 @author: Yiwei
 """
-
-seq1 = 'MLSRAVCGTSRQLAPVLAYLGSRQKHSLPDLPYDYGALEPHINAQIMQLHHSKHHAAYVNNLNVTEEKYQEALAKGDVTAQIALQPALKFNGGGHINHSIFWTNLSPNGGGEPKGELLEAIKRDFGSFDKFKEKLTAASVGVQGSGWGWLGFNKERGHLQIAACPNQDPLQGTTGLIPLLGIDVWEHAYYLQYKNVRPDYLKAIWNVINWENVTERYMACKK'
-seq2 = 'MLCRAACSTGRRLGPVAGAAGSRHKHSLPDLPYDYGALEPHINAQIMQLHHSKHHAAYVNNLNATEEKYHEALAKGDVTTQVALQPALKFNGGGHINHTIFWTNLSPKGGGEPKGELLEAIKRDFGSFEKFKEKLTAVSVGVQGSGWGWLGFNKEQGRLQIAACSNQDPLQGTTGLIPLLGIDVWEHAYYLQYKNVRPDYLKAIWNVINWENVTERYTACKK'
-seq3 = 'WNGFSEWWTHEVDYNQKLTIENNQRPKIHEHEQWGLRQSPPPPKLCCPTCQMCERMRHQNRFAPLMEVGCRCMCWFHDWWVISVGTWLHTVIMYMMWPKRFHHNECPKACFRTTYTRKNHHALYWMLFEMCCYDQDVVWSKTHIFTTVRDIEVYVEQVFFIWGPLCHVAIACYEPVKTIRRRIPMYLCRHCIRGDNSYLLACCSIIYYFYHHMSYYGVLDIL'
-
-from Bio.SubsMat import MatrixInfo
-BLOSUM62 = MatrixInfo.blosum62
-
-
-def compare(seqa, seqb):
-    score=0 
-    for i in range(0,len(seqa)):
-        if (seqa[i], seqb[i]) in BLOSUM62:
-            a=BLOSUM62[seqa[i],seqb[i]]
-            score += a
-        else:
-            a=BLOSUM62[seqb[i],seqa[i]]
-            score += a
-    return score
-
-print('The score for two sequences is', compare(seq1, seq2))
-
-
-
- 
+fhand=open('BLOUSM62.txt', 'r')
+content=fhand.read()
+lines=content.strip().split('\n')
+header=lines.pop(0)
+columns=header.split()
+matrix={}
+for i in lines:
+    entries=i.split()
+    row=entries.pop(0)
+    matrix[row]={}
+    for column in columns:
+        matrix[row][column]=entries.pop(0)
