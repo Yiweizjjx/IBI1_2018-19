f78573e48470378ca4d41a26196784fb4ecd7fdd
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 08:58:23 +0800

    add comments

--- practical7/24points.py
+++ practical7/24points.py
@@ -4,40 +4,78 @@ Created on Wed Apr  3 09:08:42 2019
 
 @author: Yiwei
 """
-
-
 import re
-from operator import mul, sub, add
- 
- 
-def div(a, b):
-    if b == 0:
-        return 999999.0
-    return a / b
- 
-ops = {mul: '*', div: '/', sub: '-', add: '+'}
- 
-def solve24(num, how, target):
-    if len(num) == 1:
-        int(num)
-        if round(num[0], 5) == round(target, 5):
-            yield str(how[0]).replace(',', '').replace("'", '')
-    else:
-        for i, n1 in enumerate(num):
-            for j, n2 in enumerate(num):
-                if i != j:
-                    for op in ops:
-                        new_num = [n for k, n in enumerate(num) if k != i and k != j] + [op(n1, n2)]
-                        new_how = [h for k, h in enumerate(how) if k != i and k != j] + [(how[i], ops[op], how[j])]
-                        yield from solve24(new_num, new_how, target)
- 
-# input numbers
-str_number=input("Please input numbers to computer 24:(use ',' to divide them)" )
-L1=re.split(r',',str_number)
-print(L1)
-for nums in L1:
-    print(nums, end=' : ')
-    try:
-        print(next(solve24(nums, nums, 24)))
-    except StopIteration:
-        print("No solution found")
+from fractions import Fraction
+#The input numbers must be interger between 1 and 23
+re_numtest = re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)') # 1-9, 10-19, 20-23
+i = 1
+while i:
+    i = 0
+    data = input('Please input numbers to computer 24:(use \',\' to divide them)\n')
+    numList = data.split(',')
+    for char in numList:
+        if re_numtest.match(char): 
+            continue
+        else: 
+            print('The input number must be intergers from 1 to 23')
+            i = 1
+            break
+
+num = list(map(int,numList)) #map:change every element into integer
+#recursion times
+count = 0 
+
+#n is len(num) 
+def dfs(n):
+    global count
+    count = count +1
+    
+    if n == 1:
+        if(float(num[0])==24):
+            return 1
+        else:
+            return 0
+    
+    #select two different numbers
+    for i in range(0,n):
+        for j in range(i+1,n):
+            a = num[i]
+            b = num[j]
+            num[j] = num[n-1]
+            # merging them all available operands 
+            num[i] = a+b
+            if(dfs(n-1)):
+                return 1
+            
+            num[i] = a-b
+            if(dfs(n-1)):
+                return 1  
+            
+            num[i] = b-a
+            if(dfs(n-1)): 
+                return 1                                                                     
+            
+            num[i] = a*b
+            if(dfs(n-1)): 
+                return 1  
+            
+            if a:
+                #floats are not precise
+                num[i] = Fraction(b,a)
+                if(dfs(n-1)): 
+                    return 1 
+                
+            if b:
+                num[i] = Fraction(a,b)
+                if(dfs(n-1)): 
+                    return 1 
+            #Backtracking  
+            num[i] = a
+            num[j] = b
+    return 0 
+
+if (dfs(len(num))): 
+    print('Yes')
+else: 
+    print('No')
+print('Recursion times:',count)
