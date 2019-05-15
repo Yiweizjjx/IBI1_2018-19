b563cfbbc71e61a36a8f0b44092e9334d29241a1
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 21:02:13 +0800

    improvements for codes

--- practical8/find go terms.py
+++ practical8/find go terms.py
@@ -4,34 +4,22 @@ Created on Wed Apr 10 09:04:54 2019
 
 @author: Yiwei
 """
-
 import xml.dom.minidom
 import re
 import pandas as pd
-'''
+import os
+os.chdir('D:\IBI1\practical\IBI1_2018-19\practical8') # change the working directory
+
+# open the file
+# create the DOM tree
 DOMTree = xml.dom.minidom.parse("go_obo.xml")
 collection = DOMTree.documentElement 
-defstr = collection.getElementsByTagName("defstr")
-i=0
-L1=[]
-L2=[]
-for i in range(0,len(defstr)):
-   a=defstr.item(i)
-   b=a.childNodes[0].data
-   if re.search(r'autophagosome+', b):
-      L1.append(b)
-      id_1=a.parentNode.parentNode.parentNode.getElementsByTagName('id')
-      id_value = id_1.childNodes[0].data
-      L2.append(id_value)
-   i = i+1
-print(L1)
-print(L2)
-'''
-###
+go = collection.getElementsByTagName("term")
+
+# define the function to find childnodes
 def Child(id, resultSet):
     for t in go:
-        parents = t.getElementsByTagName('is_a')
-# parents is a list of elements        
+        parents = t.getElementsByTagName('is_a') # parents is a list of elements        
         geneid = t.getElementsByTagName('id')[0].childNodes[0].data
         for parent in parents:
             if parent.childNodes[0].data == id:
@@ -40,10 +28,6 @@ def Child(id, resultSet):
 # create a pandas.Dataframe to store the output               
 df = pd.DataFrame(columns=['id','name','definition','childnodes'])
 
-#create the DOM tree
-DOMTree = xml.dom.minidom.parse("go_obo.xml")
-collection = DOMTree.documentElement 
-go = collection.getElementsByTagName("term")
 for term in go:
     defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
     # find terms that contain the word 'autophagosome'
@@ -52,11 +36,11 @@ for term in go:
         name = term.getElementsByTagName('name')[0].childNodes[0].data
         defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
         resultSet = set()
-        # Use set! once the item  was found,it can only be counted once
+        # Use set, because once the item was found,it can only be counted once
         # reduce redundancy
         Child(id, resultSet)
         df = df.append(pd.DataFrame({'id':[id], 'name':[name], 'definition':[defstr], 'childnodes':[len(resultSet)]}))
         print(id, len(resultSet))
 
-fname='autophagosome.xlsx'
-df.to_excel(fname, index=False)
+# save to excel
+df.to_excel('autophagosome.xlsx', index=False)
