85b05bdbe0f034e898d088478c74e2c62e45dcdd
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 21:02:03 +0800

    add comments

--- practical7/24points.py
+++ practical7/24points.py
@@ -6,10 +6,10 @@ Created on Wed Apr  3 09:08:42 2019
 """
 import re
 from fractions import Fraction
-#The input numbers must be interger between 1 and 23
+# The input numbers must be interger between 1 and 23
 re_numtest = re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)') # 1-9, 10-19, 20-23
 i = 1
-while i:
+while i: # if i is not equal to 0,then loop
     i = 0
     data = input('Please input numbers to computer 24:(use \',\' to divide them)\n')
     numList = data.split(',')
@@ -21,22 +21,22 @@ while i:
             i = 1
             break
 
-num = list(map(int,numList)) #map:change every element into integer
-#recursion times
+num = list(map(int,numList)) # map:change every element into integer
+# recursion times
 count = 0 
 
-#n is len(num) 
+# n is len(num) 
 def dfs(n):
     global count
     count = count +1
     
     if n == 1:
         if(float(num[0])==24):
-            return 1
+            return 1 
         else:
-            return 0
+            return 0 
     
-    #select two different numbers
+    # select two different numbers
     for i in range(0,n):
         for j in range(i+1,n):
             a = num[i]
@@ -60,7 +60,7 @@ def dfs(n):
                 return 1  
             
             if a:
-                #floats are not precise
+                # floats are not precise
                 num[i] = Fraction(b,a)
                 if(dfs(n-1)): 
                     return 1 
@@ -69,10 +69,10 @@ def dfs(n):
                 num[i] = Fraction(a,b)
                 if(dfs(n-1)): 
                     return 1 
-            #Backtracking  
+            # Backtracking  
             num[i] = a
             num[j] = b
-    return 0 
+    return 0 # found a 24 point solution
 
 if (dfs(len(num))): 
     print('Yes')
