e6b853bf566946aa07ac1d83eae8012b3957d641
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-15 22:01:26 +0800

    read sequences

--- practical9/Alignment.py
+++ practical9/Alignment.py
@@ -4,6 +4,7 @@ Created on Wed Apr 17 09:12:01 2019
 
 @author: Yiwei
 """
+# read BLOUSM62 matrix
 fhand=open('BLOUSM62.txt', 'r')
 content=fhand.read()
 lines=content.strip().split('\n')
@@ -16,3 +17,14 @@ for i in lines:
     matrix[row]={}
     for column in columns:
         matrix[row][column]=entries.pop(0)
+
+# read three sequences
+fh1=open('seq_human.txt', 'r')
+seq_human=fh1.read()
+
+fh2=open('seq_mouse.txt', 'r')
+seq_mouse=fh2.read()
+
+fh3=open('seq_random.txt', 'r')
+seq_random=fh3.read()
+
