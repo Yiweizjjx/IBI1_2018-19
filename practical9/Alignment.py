603c7edafe2431b85e577bb044225d7a215449ac
Yiwei <yiweiz.18@intl.zju.edu.cn>
2019-05-16 09:51:22 +0800

    improvements for codes

--- practical9/Alignment.py
+++ practical9/Alignment.py
@@ -5,7 +5,7 @@ Created on Wed Apr 17 09:12:01 2019
 @author: Yiwei
 """
 # read BLOUSM62 matrix
-fhand=open('BLOUSM62.txt', 'r')
+fhand=open('BLOSUM62.txt', 'r')
 content=fhand.read()
 lines=content.strip().split('\n')
 header=lines.pop(0)
@@ -28,3 +28,17 @@ seq_mouse=fh2.read()
 fh3=open('seq_random.txt', 'r')
 seq_random=fh3.read()
 
+def compare(seq1,seq2):
+    seq1=list(seq1)
+    score=0
+    diff=0
+    for i in range(len(seq1)):
+        x=matrix[seq1[i]][seq2[i]]
+        score+=int(x)
+        if seq1[i] != seq2[i]:
+            diff+=1
+    print(seq1,'\n', seq2)
+    print('BLOSUM score:', score)
+    print('Normalised BLOSUM score:', score/len(seq1))
+    print('Identity:', '{:.2%}'.format(1-diff/len(seq1)))
+compare(seq_human,seq_mouse)
